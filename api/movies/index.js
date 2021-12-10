d7d3e200a2db2f72ae7bc9bf95047ae918b9b3bf
Tony JinHuiLiu <20090185@mail.wit.ie>
2021-12-10 19:11:54 +0000

    Topic 11 - Set up

--- api/movies/index.js
+++ api/movies/index.js
@@ -21,18 +21,20 @@ router.get('/:id', asyncHandler(async (req, res) => {
     }
 }));
 
-// get movie reviews
-router.get('/:id', (req,res) => {
-    const id = parseInt(req.params.id);
-    if (movieReviews.id == id) {
-        res.status(200).json(movieReviews);
-    } else {
-        res.status(404).json({
-            message: 'The resource you requested could not be found.',
-            status_code: 404
-        });
-    }
-});
+router.get('/', asyncHandler(async (req, res) => {
+    let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
+    [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+
+    const totalDocumentsPromise = movieModel.estimatedDocumentCount(); //Kick off async calls
+    const moviesPromise = movieModel.find().limit(limit).skip((page - 1) * limit);
+
+    const totalDocuments = await totalDocumentsPromise; //wait for the above promises to be fulfilled
+    const movies = await moviesPromise;
+
+    const returnObject = { page: page, total_pages: Math.ceil(totalDocuments / limit), total_results: totalDocuments, results: movies };//construct return Object and insert into response object
+
+    res.status(200).json(returnObject);
+}));
 // post movie review
 router.post('/:id/reviews', (req, res) => {
     const id = parseInt(req.params.id);

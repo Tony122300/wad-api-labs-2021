46a91cef8ab429f60defa60ca4ba3e105c452835
Tony JinHuiLiu <20090185@mail.wit.ie>
2021-12-10 18:24:31 +0000

    Mongo lab Exercises

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ import './seedData'
 import usersRouter from './api/users';
 dotenv.config();
 
+const errHandler = (err, req, res, next) => {
+    /* if the error in development then send stack trace to display whole error,
+    if it's in production then just send error message  */
+    if(process.env.NODE_ENV === 'production') {
+      return res.status(500).send(`Something went wrong!`);
+    }
+    res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+  };
+
 const app = express();
 
 const port = process.env.PORT;
@@ -13,6 +22,9 @@ const port = process.env.PORT;
 app.use('/api/movies', moviesRouter);
 app.use(express.json);
 app.use('/api/users', usersRouter);
+
+app.use(errHandler);
+
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
--- seedData/index.js
+++ seedData/index.js
@@ -1,7 +1,8 @@
 import userModel from '../api/users/userModel';
+import genreModel from '../api/genres/genreModel';
 import users from './users';
+import genres from './genres';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 // deletes all user documents in collection and inserts test data
@@ -15,7 +16,18 @@ async function loadUsers() {
     console.error(`failed to Load user Data: ${err}`);
   }
 }
+async function loadGenres() {
+    try{
+      await genreModel.deleteMany();
+      await genreModel.collection.insertMany(genres);
+      console.info(`${genres.length} genres were successfully stored.`);
+    } 
+    catch (e) {
+      console.log(`failed to load genres: ${e}`);
+    }
+}
 
 if (process.env.SEED_DB) {
   loadUsers();
-}
+  loadGenres();
+} 
